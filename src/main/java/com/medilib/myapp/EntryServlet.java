1b5a1fe2e0a64a0b3f658eb477b48ed155bca123
TahleeJ <tahlee.jaynes@gmail.com>
2020-10-03 19:17:03 +0000

    Add Google auth and beginning data storage

--- src/main/java/com/medilib/myapp/EntryServlet.java
+++ src/main/java/com/medilib/myapp/EntryServlet.java
@@ -2,10 +2,22 @@ package com.medilib.myapp;
 
 import java.io.IOException;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.extensions.appengine.http.UrlFetchTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import java.security.GeneralSecurityException;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import java.util.Collections;
 
 @WebServlet(name = "EntryPortal", value = "/")
 public class EntryServlet extends HttpServlet {
@@ -14,5 +26,61 @@ public class EntryServlet extends HttpServlet {
             throws IOException {
 
         response.sendRedirect("static/entryportal.html");
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        
+        HttpTransport transport = new UrlFetchTransport();
+        JsonFactory jsonFactory = new JacksonFactory();
+        
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(transport, jsonFactory)
+            // Specify the CLIENT_ID of the app that accesses the backend:
+            .setAudience(Collections.singletonList("101678558796-he7teotdh66mk6s4hrdea62u8e4lj8hk.apps.googleusercontent.com"))
+            // Or, if multiple clients access the backend:
+            //.setAudience(Arrays.asList(CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3))
+            .build();
+
+        String idTokenString = request.getParameter("id-token");
+
+        GoogleIdToken idToken = null;
+        
+        try {
+            idToken = verifier.verify(idTokenString);
+        } catch (GeneralSecurityException e) {
+            System.out.println("auth error");
+        }
+
+        if (idToken != null) {
+        Payload payload = idToken.getPayload();
+
+        // Print user identifier
+        String userId = payload.getSubject();
+        System.out.println("User ID: " + userId);
+
+        // Get profile information from payload
+        String email = payload.getEmail();
+        boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
+        String name = (String) payload.get("name");
+        String pictureUrl = (String) payload.get("picture");
+        String locale = (String) payload.get("locale");
+        String familyName = (String) payload.get("family_name");
+        String givenName = (String) payload.get("given_name");
+
+        // Use or store profile information
+        // ...
+
+        String json = "{";
+        json += "name: " + name;
+        json += "}";
+
+        System.out.println(json);
+        
+        response.setContentType("application/json;");
+        response.getWriter().println(json);
+
+        } else {
+        System.out.println("Invalid ID token.");
+        }
     }
 }
--- src/main/webapp/static/script.js
+++ src/main/webapp/static/script.js
@@ -9,3 +9,37 @@ function goDoctor() {
 function goEntry() {
     location.replace("entryportal.html");
 }
+
+// Attempt at solving cookie issue
+// document.cookie = "HSID=27000; SSID=27000; APISID=27000; SAPISID=27000; SID=27000; SIDCC=27000; ACCOUNT_CHOOSER=27000; LSID=27000; Host_GAPS; Secure";
+
+function init() {
+  gapi.load('auth2', function() {
+    /* Ready. Make a call to gapi.auth2.init or some other API */
+    auth2 = gapi.auth2.init({client_id: '101678558796-he7teotdh66mk6s4hrdea62u8e4lj8hk.apps.googleusercontent.com'})
+  });
+}
+
+async function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+    var id_token = googleUser.getAuthResponse().id_token;
+
+    call = await fetch("/?idtoken=" + id_token, {method: "POST"});
+    response = await call.json();
+
+    console.log("test");
+    console.log("test" + response);    
+    
+    // var xhr = new XMLHttpRequest();
+    // xhr.open('POST', 'https://yourbackend.example.com/tokensignin');
+    // xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    // xhr.onload = function() {
+    // console.log('Signed in as: ' + xhr.responseText);
+    // };
+    // xhr.send('idtoken=' + id_token);
+}
